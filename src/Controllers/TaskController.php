0ba01a766892d9b8e94420f2a50bbce59deaa038
Vladislav <akhmatov.vladislav@mail.ru>
2025-02-08 12:24:31 +0500

    08/02/2025

--- src/Controllers/TaskController.php
+++ src/Controllers/TaskController.php
@@ -1,31 +1,39 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Connect;
 use App\helper;
 use PDO;
 
 class TaskController extends Connect
 {
-    public function taskById($task_id){
+    public function taskById($task_id)
+    {
         $query = $this->db->prepare("SELECT * FROM tasks WHERE task_id = :task_id");
         $query->bindParam(":task_id", $task_id, PDO::PARAM_INT);
         $query->execute();
         return $query->fetch(PDO::FETCH_OBJ);
     }
-    public function taskByUserId($user_id){
+
+    public function taskByUserId($user_id)
+    {
         $query = $this->db->prepare("SELECT * FROM tasks WHERE user_id = :user_id");
         $query->bindParam(":user_id", $user_id);
         $query->execute();
         return $query->fetch(PDO::FETCH_OBJ);
     }
-    public function tasksByUserId($user_id){
+
+    public function tasksByUserId($user_id)
+    {
         $query = $this->db->prepare("SELECT * FROM tasks WHERE user_id = :user_id");
         $query->bindParam(":user_id", $user_id);
         $query->execute();
         return $query->fetchAll(PDO::FETCH_OBJ);
     }
-    public function saveTask($user_id, $task_text, $date_begin, $date_end){
+
+    public function saveTask($user_id, $task_text, $date_begin, $date_end)
+    {
         $query = $this->db->prepare("INSERT INTO tasks (user_id, task_text, date_begin, date_end) VALUES(:user_id, :task_text, :date_begin, :date_end)");
         $formatingDateBegin = helper::formatDateForDB($date_begin);
         $formatingDateEnd = helper::formatDateForDB($date_end);
@@ -36,13 +44,15 @@ class TaskController extends Connect
         return $query->execute();
     }
 
-    public function deleteTask($task_id){
+    public function deleteTask($task_id)
+    {
         $query = $this->db->prepare("DELETE FROM tasks WHERE task_id = :task_id");
         $query->bindParam(":task_id", $task_id);
         return $query->execute();
     }
 
-    public function editTask($task_id, $task_text){
+    public function editTask($task_id, $task_text)
+    {
         $query = $this->db->prepare("UPDATE tasks SET task_text = :task_text WHERE task_id = :task_id");
         $query->bindParam(":task_id", $task_id);
         $query->bindParam(":task_text", $task_text);
